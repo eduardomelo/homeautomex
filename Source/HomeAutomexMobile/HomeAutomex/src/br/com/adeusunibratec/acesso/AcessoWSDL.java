0b65cfb900822ee84bf3ef9393d5d20394c58f56
alberico.rodrigo@gmail.com <alberico.rodrigo@gmail.com@a6b99046-1fac-42fd-2948-704d6832b915>
2014-03-07 23:20:54 +0000

    [alberico colaço]Organização das classes.

---
+++ Source/HomeAutomexMobile/HomeAutomex/src/br/com/adeusunibratec/acesso/AcessoWSDL.java
@@ -0,0 +1,12 @@
+package br.com.adeusunibratec.acesso;
+
+public class AcessoWSDL {
+	
+	private static final String NAMESPACE = "http://arcls.com.br/";
+	private static String URL = "http://192.168.56.1:7001/servico_olamundo/servico_olamundo?WSDL";
+	private static final String SOAP_ACTION = "";
+	private static final String METHOD_NAME = "getFornecedores";
+	
+	
+	
+}
