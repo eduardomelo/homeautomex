abea5d00cc833fa677fcee961c90adae07966589
eduardo87@gmail.com <eduardo87@gmail.com@a6b99046-1fac-42fd-2948-704d6832b915>
2014-03-20 19:34:10 +0000

    Faz a conexão com o servidor, ao término recebe os dados de resposta e espera um tempo x para reconectar.

--- Source/HomeAutomexArduino/base/base.ino
+++ Source/HomeAutomexArduino/base/base.ino
@@ -2,59 +2,80 @@
 #include <Ethernet.h>
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-
-char server[] = "10.1.2.6";    // name address for Google (using DNS)
-
-IPAddress ip(10,1,2,21);
+//IPAddress ip(10,1,2,21);
+IPAddress ip(192,168,0,108);
+//char server[] = "10.1.2.6";
+char server[] = "192.168.0.101";
 
 EthernetClient client;
+
+boolean fazerConexao = true;
 
 void setup() {
   Serial.begin(9600);
 
+  //try use DHCP
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
-    Serial.println(Ethernet.localIP());
     Ethernet.begin(mac, ip);
   }
+  //sem DHCP
+  //Ethernet.begin(mac, ip);
+  
   // give the Ethernet shield a second to initialize:
   delay(1000);
-  Serial.println("connecting...");
-
-  if (client.connect(server, 54090)) {
-    Serial.println("connected");
-    Serial.println(Ethernet.localIP());
-    
-    client.println("POST /HomeAutomexWS.asmx/ConsutarTodosUsuarios HTTP/1.1");
-    client.print("Host: ");
-    client.println(String(server));
-    client.println("Content-Type: application/x-www-form-urlencoded");
-    //client.println("User-Agent: Arduino/1.0");
-    client.println("Connection: close");
-    
-    String data = "param=valor&submit=Submit";
-    
-    client.print("Content-Length: ");
-    client.println(data.length());
-    client.println();
-    client.println(data);
-    client.println();
-  } 
-  else {
-    Serial.println("connection failed");
-  }
+  Serial.println("Arduino IP:");
+  Serial.println(Ethernet.localIP());
+  Serial.println();
 }
 
-void loop()
-{
+void loop() {
+  if (fazerConexao) {
+    conectar();
+    fazerConexao = false;
+  }
+  
+  //Tem cliente disponível?
   if (client.available()) {
     char c = client.read();
     Serial.print(c);
   }
 
+  //o cliente foi desconectado?
   if (!client.connected()) {
     Serial.println();
     Serial.println("disconnecting.");
     client.stop();
+    //espera 2 segundos para conectar novamente
+    delay(2000);
+    
+    fazerConexao = true;
+  }
+}
+
+void conectar() {
+  Serial.println("connecting...");
+  
+  if (client.connect(server, 80)) {
+    Serial.println("connected");
+    
+    client.println("POST /homeautomex/HomeAutomexWS.asmx HTTP/1.1");
+    client.print("Host: ");
+    client.println(String(server));
+    //client.println("Content-Type: application/x-www-form-urlencoded");
+    //client.println("User-Agent: Arduino/1.0");
+    client.println("Connection: close");
+    
+    //String data = "param=valor&submit=Submit";
+    
+    //client.print("Content-Length: ");
+    //client.println(data.length());
+    client.println("Content-Length: 0");
+    //client.println();
+    //client.println(data);
+    client.println();
+  } 
+  else {
+    Serial.println("connection failed");
   }
 }
