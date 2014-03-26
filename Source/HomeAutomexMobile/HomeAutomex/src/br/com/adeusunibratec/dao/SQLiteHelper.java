203c791328ff928ebfe87dbd24c7fdd6993a16e4
alberico.rodrigo@gmail.com <alberico.rodrigo@gmail.com@a6b99046-1fac-42fd-2948-704d6832b915>
2014-03-26 00:23:45 +0000

    Acrecimo da classe onde irá manter os dados do usuário.

--- Source/HomeAutomexMobile/HomeAutomex/src/br/com/adeusunibratec/dao/SQLiteHelper.java
+++ Source/HomeAutomexMobile/HomeAutomex/src/br/com/adeusunibratec/dao/SQLiteHelper.java
@@ -2,7 +2,6 @@ package br.com.adeusunibratec.dao;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class SQLiteHelper extends SQLiteOpenHelper {
