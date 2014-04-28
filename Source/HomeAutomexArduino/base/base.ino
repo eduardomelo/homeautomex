4eb14852dfd8699764f013280c0958e32096351f
eduardo87@gmail.com <eduardo87@gmail.com@a6b99046-1fac-42fd-2948-704d6832b915>
2014-04-28 19:19:08 +0000

    Implementada a concorrencia interruptores da casa/web no arduino

--- Source/HomeAutomexArduino/base/base.ino
+++ Source/HomeAutomexArduino/base/base.ino
@@ -1,8 +1,8 @@
 /*
 Ethernet shield w5100
-Pinos digitais 13, 12, 11 e 10 são utilizados
-Pino digital 4 é do SD reader mas como não vai ser usado, está disponível
-*/
+ Pinos digitais 13, 12, 11 e 10 são utilizados
+ Pino digital 4 é do SD reader mas como não vai ser usado, está disponível
+ */
 
 //pino digital 2 Relê 1
 //pino digital 3 Relê 2
@@ -15,153 +15,168 @@ Pino digital 4 é do SD reader mas como não vai ser usado, está disponível
 #include <ThreadController.h>
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-//IPAddress ip(10,1,2,21);
-IPAddress ip(192,168,0,108);
-//char server[] = "10.1.2.1";
-char server[] = "192.168.0.101";
+String ipString = "192.168.0.115";
+byte ip[] = { 192, 168, 0, 115 };
+char servidor[] = "192.168.0.100";
+const unsigned short int porta = 80;
+String url = "/ws/index.php";
 
-EthernetClient client;
+//IPAddress ip(10,1,2,21);
+//char servidor[] = "10.1.2.1";
+//const unsigned short int porta = 8080;
+//String url = "/ws/HomeAutomexWS.asmx";
+
+boolean usarDHCP = false;
+
+EthernetClient cliente;
 
 String readString;
 
+//dispositivos
 const unsigned short int dispositivoA = 2;
 const unsigned short int dispositivoB = 3;
 
-//const unsigned short int botaoA = 4;
-//const unsigned short int botaoB = 5;
+//botoes
+const unsigned short int botaoA = 4;
+const unsigned short int botaoB = 5;
 
+const unsigned short int intervaloBotao = 150;
+
+//interruptores digitais
 boolean interruptorA;
 boolean interruptorB;
 
 Thread threadConectar = Thread();
 Thread threadTarefas = Thread();
 
+const unsigned short int intervaloTarefas = 10;
+const unsigned short int intervaloConectar = 1000;
+
 ThreadController threadControle = ThreadController();
 
 void setup() {
   pinMode(dispositivoA, OUTPUT);
   pinMode(dispositivoB, OUTPUT);
-  //pinMode(botaoA, INPUT);
-  //pinMode(botaoB, INPUT);
+  pinMode(botaoA, INPUT);
+  pinMode(botaoB, INPUT);
+
   Serial.begin(9600);
 
-  //try use DHCP
-  /*if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
+  if (usarDHCP) {
+    if (Ethernet.begin(mac) == 0) {
+      Serial.println("Failed to configure Ethernet using DHCP");
+      Ethernet.begin(mac, ip);
+    }
+  }
+  else {
     Ethernet.begin(mac, ip);
-  }*/
-  //sem DHCP
-  Ethernet.begin(mac, ip);
-  
+  }
   // give the Ethernet shield a second to initialize:
   delay(1000);
   Serial.println("Arduino IP:");
   Serial.println(Ethernet.localIP());
   Serial.println();
-  
+
   threadTarefas.onRun(executarTarefas);
-  threadTarefas.setInterval(20);
-  
+  threadTarefas.setInterval(intervaloTarefas);
+
   threadConectar.onRun(conectar);
-  threadConectar.setInterval(2000);
-  
+  threadConectar.setInterval(intervaloConectar);
+
   threadControle.add(&threadTarefas);
   threadControle.add(&threadConectar);
 }
 
 void loop() {
-  //Tem cliente disponível?
-  if (client.available()) {
-    char c = client.read();
-    
+  if (cliente.available()) {
+    char c = cliente.read();
+
     if (readString.length() < 250) {
       readString += c;
     }
-    
+
     Serial.print(c);
   }
 
-  //o cliente foi desconectado?
-  if (!client.connected()) {
+  if (!cliente.connected()) {
     Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
+    Serial.println("Desconectado.");
+    cliente.stop();
   }
-  
+
   /*
   Run ThreadController
-  this will check every thread inside ThreadController,
-  if it should run. If yes, he will run it;
-  */
+   this will check every thread inside ThreadController,
+   if it should run. If yes, he will run it;
+   */
   threadControle.run();
 }
 
 void conectar() {
-  Serial.println("connecting...");
-  
-  //if (client.connect(server, 8080)) {
-  if (client.connect(server, 80)) {
-    Serial.println("connected");
-    
-    //client.println("POST /ws/HomeAutomexWS.asmx HTTP/1.1");
-    client.println("POST /homeautomex/index.php HTTP/1.1");
-    client.print("Host: ");
-    client.println(String(server));
-    client.println("Content-Type: application/x-www-form-urlencoded");
-    client.println("User-Agent: Arduino/1.0");
-    client.println("Connection: close");
-    
-    String data = "params=";
+  Serial.println("Conectando...");
+
+  if (cliente.connect(servidor, porta)) {
+    Serial.println("Conectado");
+
+    cliente.println("POST " + url + " HTTP/1.1");
+    cliente.print("Host: ");
+    cliente.println(String(servidor));
+    cliente.println("Content-Type: application/x-www-form-urlencoded");
+    cliente.println("User-Agent: Arduino/1.0");
+    cliente.println("Connection: close");
+
+    String data = "status=";
     data += "D2:";
-    data += digitalRead(dispositivoA);
-    //data += interruptorA;
+    data += interruptorA;
     data += "|D3:";
-    data += digitalRead(dispositivoB);
-    //data += interruptorB;
+    data += interruptorB;
+    data += "&ip=";
+    data += ipString;
     data += "&submit=Submit";
-    
-    client.print("Content-Length: ");
-    client.println(data.length());
-    client.println();
-    client.println(data);
-    client.println();
+
+    cliente.print("Content-Length: ");
+    cliente.println(data.length());
+    cliente.println();
+    cliente.println(data);
+    cliente.println();
   }
   else {
-    Serial.println("connection failed");
+    Serial.println("Falha na conexão");
   }
 }
 
 void executarTarefas() {
-  /*if (digitalRead(botaoA)) {
-    interruptorA = (interruptorA) ? LOW : HIGH;
-    delay(100);
+  if (digitalRead(botaoA)) {
+    interruptorA = (interruptorA) ? false : true;
+    delay(intervaloBotao);
   }
   else {
-    */
     if (readString.indexOf("D2:1") > 0) {
-      interruptorA = HIGH;
+      interruptorA = true;
     }
     else if (readString.indexOf("D2:0") > 0) {
-      interruptorA = LOW;
+      interruptorA = false;
     }
+  }
+
+  if (digitalRead(botaoB)) {
+    interruptorB = (interruptorB) ? false : true;
+    delay(intervaloBotao);
+  }
+  else {
+    if (readString.indexOf("D3:1") > 0) {
+      interruptorB = true;
+    }
+    else if (readString.indexOf("D3:0") > 0) {
+      interruptorB = false;
+    }
+  }
+
+  //-1 == string não encontrada.
+  //if (readString.indexOf("HTTP/1.1 200") != -1) {
+  (interruptorA) ? digitalWrite(dispositivoA, HIGH) : digitalWrite(dispositivoA, LOW);
+  (interruptorB) ? digitalWrite(dispositivoB, HIGH) : digitalWrite(dispositivoB, LOW);
   //}
-  
-  if (readString.indexOf("D3:1") > 0 && interruptorB == LOW) {
-    interruptorB = HIGH;
-  }
-  else if (readString.indexOf("D3:0") > 0 && interruptorB == HIGH) {
-    interruptorB = LOW;
-  }
-  /*else if (digitalRead(botaoB)) {
-    interruptorB = (interruptorB) ? LOW : HIGH;
-    delay(100);
-  }*/
-  
-  //-1 == string não encontrada. indexOf retorna a posição da string
-  if (readString.indexOf("HTTP/1.1 200") != -1) {
-    (interruptorA) ? digitalWrite(dispositivoA, HIGH) : digitalWrite(dispositivoA, LOW);
-    (interruptorB) ? digitalWrite(dispositivoB, HIGH) : digitalWrite(dispositivoB, LOW);
-  }
-  //Serial.println(readString);
   readString = "";
 }
+
